d40a399281e80e26fa92a95cde0f74891435c75f
anupama05-droid <anupamakj05@gmail.com>
2024-06-28 16:14:32 +0530

    logentrycompleted

--- app/src/main/java/com/anupama/entrylog/MainActivity.java
+++ app/src/main/java/com/anupama/entrylog/MainActivity.java
@@ -2,6 +2,7 @@ package com.anupama.entrylog;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -26,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences preference=getSharedPreferences("logapp",MODE_PRIVATE);
+        String username=preference.getString("user",null);
+        if (username!=null){
+            Intent i = new Intent(getApplicationContext(), LogEntry.class);
+            startActivity(i);
+        }
+
         e1=(EditText) findViewById(R.id.uname);
         e2=(EditText) findViewById(R.id.pass);
         b1=(AppCompatButton) findViewById(R.id.logbtn);
@@ -37,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
                     String getUname=e1.getText().toString();
                     String getPass=e2.getText().toString();
                     if (getUname.equals("Admin") && getPass.equals("12345")) {
+                        SharedPreferences preference=getSharedPreferences("logapp",MODE_PRIVATE);
+                        SharedPreferences.Editor editor=preference.edit();
+                        editor.putString("user","Admin");
+                        editor.apply();
                         Intent i = new Intent(getApplicationContext(), LogEntry.class);
                         startActivity(i);
                     }
